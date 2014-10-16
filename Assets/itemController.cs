dac22fb59debe48ccab07c044da08f10b38c4f2f
Reck Hou <reckhou@gmail.com>
2014-10-16 15:33:38 +0800

    Add triggers.

---
+++ Assets/itemController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemController : MonoBehaviour {
+
+	private triggerController trigger;
+
+	void Start() {
+		trigger = transform.FindChild("Trigger").GetComponent<triggerController>();
+	}
+
+	public void OnTrigger() {
+		print("Item Triggered!");
+
+		SpriteRenderer rend = GetComponent<SpriteRenderer>();
+		Color color = rend.color;
+		color.r = 0;
+		rend.color = color;
+	}
+}
---
+++ Assets/triggerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerController : MonoBehaviour {
+
+	public enum Type {
+		normal,
+		narrator
+	};
+
+	public int triggerType = (int)Type.normal;
+	public bool reusable = false;
+
+	private itemController parent;
+
+	// Use this for initialization
+	void Start () {
+		parent = transform.parent.GetComponent<itemController>();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		print("Trigger!");
+
+		if (!reusable) {
+			GetComponent<BoxCollider2D>().enabled = false;
+			this.enabled = false;
+		}
+
+		parent.OnTrigger();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
