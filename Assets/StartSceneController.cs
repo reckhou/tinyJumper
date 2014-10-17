090d81350b3264932adbb36175780c948cab643f
Reck Hou <reckhou@gmail.com>
2014-10-17 13:15:14 +0800

    Particle effect test.

---
+++ Assets/StartSceneController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneController : MonoBehaviour {
+	public float FadeTime = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			Application.LoadLevel("main");
+		}
+	}
+}
